3e8bee7263a06037e6e3ae040af5ab2099b92484
Áron Horváth <horvatharon15@gmail.com>
2020-11-26 12:40:59 +0100

    v0.2

--- canvas.js
+++ canvas.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', () =>{
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
-
+    const colorPicker = document.getElementById('colorpicker');
+    
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
     let painting = false;
-
     function startPosition(e) {
         painting = true;
         draw(e);
@@ -16,11 +16,15 @@ window.addEventListener('load', () =>{
         ctx.beginPath();
     }
 
+    function changeColor(event) {
+        console.log(event.target.value);
+        ctx.strokeStyle = event.target.value;
+    }
+
     function draw(e) {
         if(!painting) return;
         ctx.lineWidth = 10;
         ctx.lineCap = 'round';
-
         ctx.lineTo(e.clientX, e.clientY);
         ctx.stroke();
         ctx.beginPath();
@@ -30,6 +34,7 @@ window.addEventListener('load', () =>{
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', finishedPosition);
     canvas.addEventListener('mousemove', draw);
+    colorPicker.addEventListener('change', changeColor, false);
 
 
 });
